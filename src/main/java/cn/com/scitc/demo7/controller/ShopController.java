f47a58216d016e434dd99a555ac6c959429cda3c
zql <742715157@qq.com>
2019-09-26 19:25:34 +0800

    订单制作

--- src/main/java/cn/com/scitc/demo7/controller/ShopController.java
+++ src/main/java/cn/com/scitc/demo7/controller/ShopController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.awt.print.Book;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping(value = "/")
@@ -24,19 +22,23 @@ public class ShopController {
 
         String water=request.getParameter("water");
         String price=request.getParameter("price");
+        String all = water + price;
 
         ArrayList waters=(ArrayList) session.getAttribute("waters");
+        //Map<String, String> waters =(Map<String,String>) session.getAttribute("waters");
+        //map.put("id", rb.getReaderID());
+        //map.put("name", rb.getReaderName());
+        //session.setAttribute("map",map);
         if(waters==null){
            waters=new ArrayList();
             session.setAttribute("waters",waters);
         }
         if(waters!=null){
-            waters.add(water);
-            waters.add(price);
+            waters.add(all);
             for(int l = 0; l < waters.size(); l++){
                 String ss = (String)waters.get(l);
                 //model.addAttribute("ss",ss);
-                logger.info("登录名：" + ss );
+                //logger.info("登录名：" + ss );
             }
             model.addAttribute("ss",waters);
            // Enumeration enumeration =session.getAttributeNames();
@@ -46,6 +48,19 @@ public class ShopController {
 
         }
 
+
+        return "shop";
+    }
+    @RequestMapping(value = "/delete")
+    public String delete(HttpServletRequest request,String ss){
+        HttpSession session = request.getSession();
+        ArrayList waters=(ArrayList) session.getAttribute("waters");
+        waters.remove(ss);
+        for(int l = 0; l < waters.size(); l++){
+            String s = (String)waters.get(l);
+            //model.addAttribute("ss",ss);
+            logger.info("登录名：" + s );
+        }
         return "shop";
     }
 }
